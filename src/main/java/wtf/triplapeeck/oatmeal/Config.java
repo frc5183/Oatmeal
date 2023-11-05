cdb04f6c22eb1817cf542ceb16406bf7ba32afe6
Trip-kun <kaidenmcmartin1@gmail.com>
2023-11-04 22:29:19 -0400

    refactor(oatmeal): remove unused imports

--- src/main/java/wtf/triplapeeck/oatmeal/Config.java
+++ src/main/java/wtf/triplapeeck/oatmeal/Config.java
@@ -2,9 +2,6 @@ package wtf.triplapeeck.oatmeal;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.j256.ormlite.db.DatabaseType;
-
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
